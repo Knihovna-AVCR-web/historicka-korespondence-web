217a29c75863299b60ec84703018602daab182f1
Jarka Pachlová <jarka.p@gmail.com>
2020-12-17 19:54:12 +0100

    move wp filters to custom file

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once get_template_directory() . '/inc/wp-settings.php';
+
 add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     register_nav_menu('main-menu', 'Main Menu');
@@ -16,135 +18,15 @@ add_action('after_setup_theme', function () {
     require_once get_template_directory() . '/inc/breadcrumbs.php';
 });
 
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'feed_links', 2);
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'start_post_rel_link', 10, 0);
-remove_action('wp_head', 'parent_post_rel_link', 10, 0);
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
-remove_action('wp_head', 'rel_canonical', 10, 0);
-remove_action('wp_head', 'rest_output_link_wp_head');
-remove_action('wp_head', 'wp_oembed_add_discovery_links');
-remove_action('template_redirect', 'rest_output_link_header', 11, 0);
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('admin_print_scripts', 'print_emoji_detection_script');
-remove_action('wp_print_styles', 'print_emoji_styles');
-remove_action('admin_print_styles', 'print_emoji_styles');
-
-
-function hiko_remove_wp_version_strings($src)
-{
-    global $wp_version;
-
-    parse_str(parse_url($src, PHP_URL_QUERY), $query);
-
-    if (!empty($query['ver']) && $query['ver'] === $wp_version) {
-        $src = remove_query_arg('ver', $src);
-    }
-
-    return $src;
-}
-add_filter('script_loader_src', 'hiko_remove_wp_version_strings');
-add_filter('style_loader_src', 'hiko_remove_wp_version_strings');
-
-
-add_filter('the_generator', function () {
-    return '';
-});
-
-
-add_filter('style_loader_tag', function ($input) {
-    $re = "!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!";
-    preg_match_all(
-        $re,
-        $input,
-        $matches
-    );
-    if (empty($matches[2])) {
-        return $input;
-    }
-
-    $media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
-    return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
-});
-
-
-add_action('send_headers', function () {
-    header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
-
-    header('X-Frame-Options: DENY');
-
-    header('X-XSS-Protection: 1; mode=block');
-
-    header('X-Content-Type-Options: nosniff');
-}, 1);
-
 
 add_action('wp_enqueue_scripts', function () {
-    $custom_js = get_template_directory_uri() . '/assets/dist/custom.min.js';
-    $custom_js .= '?v=' . filemtime(get_template_directory() . '/assets/dist/custom.min.js');
-    $custom_css = get_template_directory_uri() . '/assets/dist/main.css';
-    $custom_css .= '?v=' . filemtime(get_template_directory() . '/assets/dist/main.css');
-    $custom_js_deps = ['lazyload'];
+    $dist_uri = get_template_directory_uri() . '/assets/dist/';
+    $dist_dir = get_template_directory() . '/assets/dist/';
 
-    wp_dequeue_script('jquery');
-    wp_deregister_script('wp-embed');
+    is_user_logged_in() ? wp_enqueue_script('jquery') : wp_dequeue_script('jquery');
 
-    wp_enqueue_script(
-        'bootstrap',
-        'https://cdn.jsdelivr.net/npm/bootstrap.native@3.0.10/dist/bootstrap-native.min.js',
-        [],
-        null,
-        true
-    );
-
-    wp_enqueue_script(
-        'lazyload',
-        'https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.1.2/dist/lazyload.min.js',
-        [],
-        null,
-        true
-    );
-
-    if (!is_front_page()) {
-        wp_enqueue_script(
-            'slimselect',
-            'https://cdn.jsdelivr.net/npm/slim-select@1.26.0/dist/slimselect.min.js',
-            [],
-            null,
-            true
-        );
-
-        wp_enqueue_script(
-            'tabulator',
-            'https://cdn.jsdelivr.net/npm/tabulator-tables@4.7.2/dist/js/tabulator.min.js',
-            [],
-            null,
-            true
-        );
-
-        $custom_js_deps = ['lazyload', 'slimselect', 'tabulator'];
-    }
-
-    wp_enqueue_script('main', $custom_js, $custom_js_deps, null, true);
-
-    wp_enqueue_style('main', $custom_css);
-});
-
-
-add_filter('body_class', function ($classes) {
-    if (is_page_template('page-templates/page-blekastad-front.php')) {
-        $classes[] = 'blekastad-front';
-    } elseif (is_page_template('page-templates/page-home.php')) {
-        $classes[] = 'main-front';
-    }
-
-    return $classes;
+    wp_enqueue_script('main', $dist_uri . 'app.js', [], filemtime($dist_dir . 'app.js'), true);
+    wp_enqueue_style('main', $dist_uri . 'app.css', [], filemtime($dist_dir . 'app.css'));
 });
 
 
@@ -165,11 +47,6 @@ add_action('template_redirect', function () {
     } else {
         exit(wp_redirect(home_url('projekty')));
     }
-});
-
-
-add_action('admin_menu', function () {
-    remove_menu_page('edit-comments.php');
 });
 
 
@@ -278,6 +155,7 @@ function get_all_posts()
     return $results;
 }
 
+
 function get_esc_setted_value($value)
 {
     if (isset($value)) {
@@ -296,16 +174,16 @@ function cmb2_output_gallery($file_list_meta_key)
     }
     ?>
     <div class="gallery">
-
         <?php foreach ((array) $files as $file_id => $file_url) : ?>
             <a href="<?= $file_url; ?>" data-caption="<?= wp_get_attachment_caption($file_id); ?>">
-                <img src="<?= wp_get_attachment_image_src($file_id, 'medium')[0]; ?>" alt="<?= wp_get_attachment_caption($file_id); ?>" class="img-fluid img-thumbnail mr-3 mb-3">
+                <img src="<?= wp_get_attachment_image_src($file_id, 'medium')[0]; ?>" alt="<?= wp_get_attachment_caption($file_id); ?>" class="mb-3 mr-3 img-fluid img-thumbnail">
             </a>
 
         <?php endforeach; ?>
     </div>
     <?php
 }
+
 
 function is_localhost($whitelist = ['127.0.0.1', '::1'])
 {
@@ -375,6 +253,7 @@ function get_encoded_mailto_link($classes)
     return ob_get_clean();
 }
 
+
 function get_content_from_url($url)
 {
     if (!is_localhost()) {
@@ -390,6 +269,7 @@ function get_content_from_url($url)
     );
 }
 
+
 function get_single_letter_meta()
 {
     $letter = null;
@@ -401,11 +281,11 @@ function get_single_letter_meta()
         $letter = json_decode($letter, true);
         $letter['document_type'] = get_letter_doc_meta($letter['document_type']);
         $letter['related_resources'] = get_letter_related_resources($letter['related_resources']);
-
     }
 
     return $letter;
 }
+
 
 function custom_format_date($day, $month, $year)
 {
@@ -419,6 +299,7 @@ function custom_format_date($day, $month, $year)
 
     return "{$day}/{$month}/{$year}";
 }
+
 
 function get_letter_object_meta($id, $name, $meta, $type = false)
 {
@@ -456,6 +337,7 @@ function format_letter_object($data, $element)
     return $result;
 }
 
+
 function get_letter_doc_meta($data)
 {
     $result = [
@@ -476,6 +358,7 @@ function get_letter_doc_meta($data)
 
     return $result;
 }
+
 
 function get_letter_related_resources($resources)
 {
