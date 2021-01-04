dd5ec51d5aecbb226723810e061b9d92f86451b0
Jarka Pachlová <jarka.p@gmail.com>
2021-01-04 23:06:00 +0100

    fix blekastad front page

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 </main>
-<footer class="py-12 mt-auto  bg-brown">
+<footer class="py-12 mt-auto bg-brown">
     <div class="container flex flex-wrap justify-between px-5 mx-auto">
         <p class="my-1 mr-5 text-sm text-white">
-            &copy; <?= date('Y') ?> – <a href="https://www.mua.cas.cz/">Masarykův ústav a Archiv AV ČR, v.&nbsp;v.&nbsp;i.</a>, <a href="https://www.flu.cas.cz/">Filosofický ústav AV ČR, v.&nbsp;v.&nbsp;i.</a>, <a href="https://www.lib.cas.cz/">Knihovna AV ČR, v.&nbsp;v.&nbsp;i.</a>
+            &copy; <?= date('Y') ?> – <a href="https://www.mua.cas.cz/" class="hover:underline">Masarykův ústav a Archiv AV ČR, v.&nbsp;v.&nbsp;i.</a>, <a href="https://www.flu.cas.cz/" class="hover:underline">Filosofický ústav AV ČR, v.&nbsp;v.&nbsp;i.</a>, <a href="https://www.lib.cas.cz/" class="hover:underline">Knihovna AV ČR, v.&nbsp;v.&nbsp;i.</a>
         </p>
         <p class="my-1 text-sm text-white">
-            <?= get_encoded_mailto_link('') ?>
+            <?= get_encoded_mailto_link('hover:underline') ?>
         </p>
     </div>
 </footer>
--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -33,7 +33,7 @@ add_action('carbon_fields_register_fields', function () {
             <p class="mb-6 leading-relaxed">
                 <?= $block['content']; ?>
             </p>
-            <a href="<?= get_permalink($block['link']); ?>" class="px-6 py-2 text-sm uppercase duration-150 border border-white hover:bg-red-700 hover:border-red-700" >
+            <a href="<?= get_permalink($block['link']); ?>" class="px-6 py-2 text-sm uppercase duration-150 border border-white rounded-sm hover:bg-red-700 hover:border-red-700" >
                 <?php _e('Více', 'hiko'); ?>&nbsp;&nbsp;&raquo;
             </a>
             <?php
