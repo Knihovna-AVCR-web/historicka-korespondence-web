57e37dfbff823baea1ac667347bca07dc7384df6
Jarka Pachlová <jarka.p@gmail.com>
2022-01-13 19:43:06 +0100

    add cookies settings button

--- footer.php
+++ footer.php
@@ -4,9 +4,14 @@
         <p class="my-1 mr-5 text-sm text-white">
             &copy; <?= date('Y') ?> – <a href="https://www.mua.cas.cz/" class="hover:underline">Masarykův ústav a Archiv AV ČR, v.&nbsp;v.&nbsp;i.</a>, <a href="https://www.flu.cas.cz/" class="hover:underline">Filosofický ústav AV ČR, v.&nbsp;v.&nbsp;i.</a>, <a href="https://www.lib.cas.cz/" class="hover:underline">Knihovna AV ČR, v.&nbsp;v.&nbsp;i.</a>
         </p>
-        <p class="my-1 text-sm text-white">
-            <?= get_encoded_mailto_link('hover:underline') ?>
-        </p>
+        <div class="text-sm text-white">
+            <p class="my-1">
+                <?= get_encoded_mailto_link('hover:underline') ?>
+            </p>
+            <p>
+                <?= do_shortcode('[cookies-button text="Cookies"]') ?>
+            </p>
+        </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
