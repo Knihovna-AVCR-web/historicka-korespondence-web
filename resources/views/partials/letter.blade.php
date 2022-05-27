be5148e2b49341655a4795bd69aa9a490fe235e1
Jarka Pachlová <jarka.p@gmail.com>
2022-05-27 18:54:52 +0200

    move copyright

--- resources/views/partials/letter.blade.php
+++ resources/views/partials/letter.blade.php
@@ -344,16 +344,10 @@
                 </tbody>
             </table>
         @endforeach
-        @if (!empty($letter['related_resources']))
+        @if (!empty($letter['related_resources']) && isset($letter['related_resources'][0]['title']))
             <h2 class="text-lg font-bold">Related resources</h2>
             <table class="w-full mb-10 text-sm">
                 <tbody>
-                    @if ($letter['copyright'])
-                        <tr class="align-baseline border-t border-b border-gray-200">
-                            <td class="w-1/5 py-2">Copyright</td>
-                            <td class="py-2">{{ $letter['copyright'] }}</td>
-                        </tr>
-                    @endif
                     <tr class="align-baseline border-t border-b border-gray-200">
                         <td class="py-2">
                             <ul class="list-disc list-inside max-w-prose">
@@ -375,6 +369,19 @@
                 </tbody>
             </table>
         @endif
+        @if (!empty($letter['copyright']))
+            <h2 class="text-lg font-bold">Copyright</h2>
+            <table class="w-full mb-10 text-sm">
+                <tbody>
+                    @if ($letter['copyright'])
+                        <tr class="align-baseline border-t border-b border-gray-200">
+                            <td class="w-1/5 py-2">Copyright</td>
+                            <td class="py-2">{{ $letter['copyright'] }}</td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+        @endif
         @isset($letter['media'])
             <div class="flex flex-wrap mb-6 -m-x-1 gallery">
                 @foreach ($letter['media'] as $media)
@@ -385,8 +392,9 @@
                 @endforeach
             </div>
         @endisset
-        <a href="{{ $dbUrl }}/edit/{{ $letter['uuid']}}" target="_blank" rel="noopener noreferrer" class="flex justify-end">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-700" fill="none" viewBox="0 0 24 24"
+        <a href="{{ $dbUrl }}/edit/{{ $letter['uuid'] }}" target="_blank" rel="noopener noreferrer"
+            class="flex justify-end">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-700" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
