40a981bac5f8f18e75a26973915203a0b5d17baf
Jarka Pachlová <jarka.p@gmail.com>
2022-05-27 18:54:59 +0200

    fix blekastad menu

--- resources/views/partials/blekastad-nav.blade.php
+++ resources/views/partials/blekastad-nav.blade.php
@@ -1,7 +1,7 @@
 @if (!empty($blekastadNavigation))
     <nav
         class="flex flex-col flex-wrap items-start justify-between w-full px-5 py-2 text-sm bg-red-700 shadow-md lg:flex-row">
-        @foreach ($navigation->toArray() as $item)
+        @foreach ($blekastadNavigation->toArray() as $item)
             @if ($loop->first)
                 <a href="{{ $item->url }}" class="flex items-center py-2 mr-6 italic text-white">
                     <div class="hidden w-16 h-auto mr-6 lg:block"></div>
@@ -10,7 +10,7 @@
             @endif
         @endforeach
         <nav class="flex flex-col lg:flex-row">
-            @foreach ($navigation->toArray() as $item)
+            @foreach ($blekastadNavigation->toArray() as $item)
                 @continue($loop->first)
                 <a href="{{ $item->url }}" class="block m-2 text-white"
                     @if (!empty($item->target)) target="{{ $children->target }}" @endif>
